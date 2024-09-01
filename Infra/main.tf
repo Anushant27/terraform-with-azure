d68d2821a591568f81d22a2f2501c51d4a95f672
Anushant27 <anushant.2k16@gmail.com>
2024-09-01 13:19:24 +0530

    Uploading terraform

---
+++ Infra/main.tf
@@ -0,0 +1,89 @@
+
+#resource "azurerm_resource_group" "rg" {
+#  name     = "${var.prefix}-rg"
+#  location = var.location
+#}
+
+resource "azurerm_storage_account" "storage_account" {
+  name                     = var.storage_account_name
+  resource_group_name      = var.rg
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+
+  static_website {
+    index_document = "index.html"
+  }
+}
+
+resource "azurerm_storage_blob" "blob" {
+  name                   = var.index_document
+  storage_account_name   = azurerm_storage_account.storage_account.name
+  storage_container_name = "$web"
+  type                   = "Block"
+  content_type           = "text/html"
+  source_content         = var.source_content
+
+}
+
+
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = var.rg
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = var.rg
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = var.location
+  resource_group_name = var.rg
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm"
+  location              = var.location
+  resource_group_name   = var.rg
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_DS1_v2"
+
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
+  }
+}
---
+++ Infra/variables.tf
@@ -0,0 +1,26 @@
+variable "location" {
+  description = "The location of the resource"
+
+}
+
+variable "prefix" {
+  description = "Name of the resource group used in the project"
+
+}
+
+variable "rg" {
+  
+}
+
+variable "storage_account_name" {
+  description = "Name of the storage account"
+}
+
+variable "source_content" {
+  description = "The content for the static webpage that is inside index.html page"
+}
+
+variable "index_document" {
+  description = "Name of index file"
+
+}
